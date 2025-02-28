84f5ebb40966f4e13b0f7fe57f4dd40ffc25aefc
geethajakka <geethjakka@gmail.com>
2025-02-28 17:50:23 +0530

    Modified syntax errors

--- ec2.tf
+++ ec2.tf
@@ -25,7 +25,7 @@ resource "aws_network_interface" "aws_network_interface_demo" {
   }
 }
 
-resource "aws_security_group" "aws_security_group_demo" {
+/*resource "aws_security_group" "aws_security_group_demo" {
   name = "${random_pet.sg.id}-sg"
   vpc_id = aws_vpc.aws_vpc_demo.id
   ingress = [
@@ -38,6 +38,42 @@ resource "aws_security_group" "aws_security_group_demo" {
 ]
 
 }
+*/
+
+resource "aws_security_group" "aws_security_group_demo" {
+  name = "${random_pet.sg.id}-sg"
+  vpc_id = aws_vpc.aws_vpc_demo.id
+  description = "Allow inbound traffic on port 8080"
+
+  ingress = [
+    {
+      description      = "Allow HTTP traffic"
+      from_port        = 8080
+      to_port          = 8080
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress = [
+    {
+      description      = "Allow all outbound traffic"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+}
+
 
 resource "aws_instance" "aws_ec2_demo" {
   ami = "ami-0d682f26195e9ec0f"
